83dbf59d8cabff43baf71269e153c7cf5b3babb3
Fabio dp <dipane.fabio@gmail.com>
2020-01-17 12:20:37 +0100

    Add project files.

---
+++ CSV-Manipulator-Server/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace CSV_Manipulator_Server.Models
+{
+	public class ErrorViewModel
+	{
+		public string RequestId { get; set; }
+
+		public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+	}
+}
